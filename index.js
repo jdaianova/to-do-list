a4bbc0838a5c1ea4ff3173fc28b23544d7e746a7
Julia <jdaianova@gmail.com>
2022-06-22 18:19:05 +0200

    commit 22.06 number_1

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ const inputTodo = document.querySelector('input');
 const list = document.querySelector('ul');
 
 buttonSubmit.addEventListener('click', () => {
-    const listElement =  document.createElement('li');
+    const listElement = document.createElement('li');
     listElement.innerText = inputTodo.value;
-    list.appendChild(listElement);
-    listElement.classList.add('todo-list-element');
+
+    if (listElement.innerText != '') {
+        list.appendChild(listElement);
+        listElement.classList.add('todo-list-element');
+        inputTodo.value = "";
+    
+        listElement.addEventListener('click', () => {
+            listElement.classList.add('todo-list-element-complete');
+        });
+    }
+
+    listElement.addEventListener('dblclick', () => {
+        listElement.classList.remove('todo-list-element-complete', 'todo-list-element');
+        listElement.innerText = '';
+    });
 });
